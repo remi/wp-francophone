c782cac2b138fd58813948b8a4755f9de0149083
remiprev <remi@exomel.com>
2010-01-03 08:59:19 -0500

    added some doc and cleaned some whitespace

--- wp-francophone.php
+++ wp-francophone.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: WP_Francophone
 Plugin URI: http://github.com/remiprev/wp-francophone/
-Version: 0.1
+Version: 0.15
 Description: A small WordPress plugin to add a few french-specific filters and actions.
 Author: Rémi Prévost
 Author URI: http://remiprevost.com
@@ -11,11 +11,12 @@ Author URI: http://remiprevost.com
 /**
  * @classname WP_Francophone
  * @author remi (exomel.com)
- * @version 20100101
+ * @version 20100103
  */
 class WP_Francophone {
 
 	/**
+	 * Set filters and actions
 	 * @constructor
 	 */
 	function WP_Francophone() {
@@ -26,11 +27,12 @@ class WP_Francophone {
 	}
 
 	/**
+	 * Turns "1 janvier 2010" to "1<sup>er</sup> janvier 2010"
 	 * @param string $date
-	 * return string
+	 * @return string
 	 */
 	function date_i18n( $date ) {
-		return preg_replace('/(\s|^)1\s/','1<sup>er</sup> ',$date);
+		return preg_replace( '/(\s|^)1\s/', '1<sup>er</sup> ', $date );
 	}
 
 }
